a4dbbaf0a4e2fb841bb83ca9f3f9bf0684122f31
Harshit <harshitkumarsaini88@gmail.com>
2021-05-17 16:19:19 +0530

    final bot

--- covid.py
+++ covid.py
@@ -1,26 +1,24 @@
 import requests
 from datetime import datetime
 from pushbullet import PushBullet
-import time
 
 count=0
 API= 'o.WfYU9eM5o7fXoBq5PxburlwjXEpfCw0Q'
 pb=PushBullet(API)
 #SHAHDARA
 b=datetime.today()
-headers = {"User-Agent": "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"}
+headers = {"User-Agent":"BF-DirectLine/3.0 (Microsoft-BotFramework/3.0 +https://botframework.com/ua)"}
 url='https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?'
 para={"district_id": 148, "date": b.strftime("%d-%m-%Y")}
 a=requests.get(url,params=para,headers=headers)
 file=a.json()
 
 
-while True:
-	for i in range(0,len(file['centers'])):
-		if(file['centers'][i]['sessions'][0]['min_age_limit']==45 and (file['centers'][i]['sessions'][0]['available_capacity']>=1)):
-			count=count+1
 
-	if count>=1:
-		pb.push_note("Vaccine Slots Available", "Slots Available for 18+ Hurry up")
+for i in range(0,len(file['centers'])):
+    if(file['centers'][i]['sessions'][0]['min_age_limit']==45 and (file['centers'][i]['sessions'][0]['available_capacity']>=1)):
+        count=count+1
 
-	time.sleep(60)
+if count>=1:
+    pb.push_note("Vaccine Slots Available", "Slots Available for 18+ Hurry up")
+
