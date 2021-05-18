630c6a8d176681e410c92923f6f8f9803bda2978
Harshit <harshitkumarsaini88@gmail.com>
2021-05-18 11:55:42 +0530

    testing Covid

--- covid.py
+++ covid.py
@@ -1,39 +1,11 @@
-import requests
-from datetime import datetime
-from pushbullet import PushBullet
-import json
+from pushbullet import Pushbullet
+import time
 
-count=0
+
 API= 'o.WfYU9eM5o7fXoBq5PxburlwjXEpfCw0Q'
-pb=PushBullet(API)
-#SHAHDARA
-b=datetime.today()
-headers = {"Accept-Language": "hi_IN","User-Agent":"Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"}
-url='https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?'
-para={"district_id": 148, "date": b.strftime("%d-%m-%Y")}
-a=requests.get(url,params=para,headers=headers)
-fi=a.text
-print(fi)
+pb=Pushbullet(API)
+while True:
 
+    pb.push_note("Hurry up dude","working succesfully")
 
-try:
-    with open("file1.json",'w') as f:
-        f.write(fi)
-
-    with open("file1.json",'r') as f:
-        file= json.load(f)
-
-    for i in range(0, len(file['centers'])):
-        if (file['centers'][i]['sessions'][0]['min_age_limit'] == 45 and (
-                file['centers'][i]['sessions'][0]['available_capacity'] >= 1)):
-            count = count + 1
-
-    if count >= 1:
-        pb.push_note("Vaccine Slots Available", "Slots Available for 18+ Hurry up")
-except Exception as e:
-    print("No json Data found on server")
-
-
-
-
-
+    time.sleep(5)
