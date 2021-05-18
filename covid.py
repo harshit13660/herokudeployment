4074b5125be427acdfdebf3537e531e9671e1eaf
Harshit <harshitkumarsaini88@gmail.com>
2021-05-18 15:26:28 +0530

    Final Update

--- covid.py
+++ covid.py
@@ -1,11 +1,48 @@
-from pushbullet import Pushbullet
+import requests
+from datetime import datetime
+from pushbullet import PushBullet
 import time
 
 
+
 API= 'o.WfYU9eM5o7fXoBq5PxburlwjXEpfCw0Q'
-pb=Pushbullet(API)
+pb=PushBullet(API)
+#SHAHDARA
+b=datetime.today()
+
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"}
+
+url='https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?'
+
+para={"district_id": 148, "date": b.strftime("%d-%m-%Y")}
+
+proxi={
+    "https": "14.140.131.82:3128"
+}
+
 while True:
+    count = 0
+    try:
+        a=requests.get(url,params=para,headers=headers,proxies=proxi)
+        file = a.json()
 
-    pb.push_note("Hurry up dude","working succesfully")
+        for i in range(0, len(file['centers'])):
+            if(file['centers'][i]['sessions'][0]['min_age_limit'] == 45 and (file['centers'][i]['sessions'][0]['available_capacity'] >= 1)):
+                count = count + 1
 
-    time.sleep(5)
+        if count >= 1:
+            pb.push_note("Vaccine Slots Available", "Slots Available for 18+ Hurry up")
+
+    except Exception as e:
+        print(e)
+
+    time.sleep(120)
+
+
+
+
+
+
+
+
+
